656fe8bdfe63080d2ae19f6f7df6be24415afa9b
SeeSmile <1092443178@qq.com>
2016-07-14 18:18:28 +0800

    init
    init

---
+++ PythonApi/app/src/main/java/com/example/fupei/pythonapi/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.fupei.pythonapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.et_account)
+    EditText et_account;
+    @BindView(R.id.et_password)
+    EditText et_password;
+    @BindView(R.id.tv_login)
+    TextView tv_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initToolBar();
+        initListener();
+    }
+
+    private void initListener() {
+        tv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initToolBar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setTitle("");
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.mipmap.icon_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
