1dc83d4053eeadcc96e3ac3c329166b2d7408680
SeeSmile <1092443178@qq.com>
2016-07-14 19:20:52 +0800

    fff
    fff

--- PythonApi/app/src/main/java/com/example/fupei/pythonapi/MainActivity.java
+++ PythonApi/app/src/main/java/com/example/fupei/pythonapi/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.fupei.pythonapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     EditText et_password;
     @BindView(R.id.tv_login)
     TextView tv_login;
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +35,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListener() {
+        tv_login = (TextView) findViewById(R.id.tv_login);
         tv_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showToast();
             }
         });
     }
 
     private void initToolBar() {
+
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setTitle("");
         setSupportActionBar(mToolbar);
@@ -50,5 +56,21 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void showToast() {
+        mToast = new Toast(this);
+        TextView tv = new TextView(this);
+        tv.setText("我曹嗷嗷");
+        TranslateAnimation ani = new TranslateAnimation(0, 0, 100, 100);
+        ani.setDuration(1000);
+//        tv.setAnimation(ani);
+        mToast.setView(tv);
+        mToast.setGravity(Gravity.TOP, 0, 0);
+        mToast.setDuration(Toast.LENGTH_SHORT);
+        mToast.show();
+        mToast.getView().startAnimation(ani);
+
+//        tv.startAnimation(ani);
     }
 }
